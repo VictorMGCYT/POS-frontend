d1f1aeea86aacb68ed7ebb8795c4acdde66a21f0
VictorMGCYT <manuelvgc2233@gmail.com>
2025-05-11 14:16:47 -0600

    Adding tailwindcss y shadcn
    11-05-2025

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // Usa 'media' si prefieres que siga la configuración del sistema
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
