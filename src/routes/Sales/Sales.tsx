c27ba1cfe0885ea2599bf307be2a9c8164c83a7e
VictorMGCYT <manuelvgc2233@gmail.com>
2025-05-13 20:47:15 -0600

    Searcher of products
    14-05-2025

--- src/routes/Sales/Sales.tsx
+++ src/routes/Sales/Sales.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { SearchIcon } from "lucide-react";
 
 
@@ -18,13 +21,52 @@ export default function Sales(){
                     </p> */}
                 </div>
             </div>
-            <div className="grid p-4 grid-cols-1 md:grid-cols-2">
-                <p>
-                    <div className="flex items-center w-full max-w-sm space-x-2 rounded-lg border dar:border-slate-700 bg-gray-50 dark:bg-slate-950 px-3.5 py-2">
-                        <SearchIcon className="h-4 w-4" />
-                        <Input type="search" placeholder="Buscar" className="w-full border-0 h-6 font-semibold" />
-                    </div>
-                </p>
+            <div className="grid p-4 grid-cols-1 md:grid-cols-2 gap-2">
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-semibold">
+                            Buscar Productos
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex items-center w-full space-x-2 rounded-lg border dar:border-slate-700 bg-gray-50 dark:bg-slate-950 px-3.5 py-2">
+                            <SearchIcon className="h-4 w-4" />
+                            <Input 
+                            type="search" 
+                            placeholder="Buscar producto por nombre o código de barras" 
+                            className="w-full border-0 h-6 font-semibold" />
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <div className="w-full rounded-md border">
+                            <Table>
+                                <TableHeader>
+                                    <TableRow className="dark:hover:bg-slate-900">
+                                        <TableHead className="w-[100px]">Producto</TableHead>
+                                        <TableHead>Precio</TableHead>
+                                        <TableHead>Stock</TableHead>
+                                        <TableHead>Tipo</TableHead>
+                                        <TableHead></TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow className="dark:hover:bg-slate-900">
+                                        <TableCell className="font-medium">INV001</TableCell>
+                                        <TableCell>Paid</TableCell>
+                                        <TableCell>Credit Card</TableCell>
+                                        <TableCell>$250.00</TableCell>
+                                        <TableCell>
+                                            <Button variant={"outline"}>
+                                                +
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </CardFooter>
+                </Card>
+                
                 <p>
                     dos
                 </p>
