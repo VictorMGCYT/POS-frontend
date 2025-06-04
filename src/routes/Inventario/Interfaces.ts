a8ac25659b54bfa84bb7d9999410080d423fe6b0
VictorMGCYT <manuelvgc2233@gmail.com>
2025-06-03 21:25:19 -0600

    form by add product
    03-05-2025

---
+++ src/routes/Inventario/Interfaces.ts
@@ -0,0 +1,8 @@
+export interface AddProductInterface {
+    name:          string;
+    skuCode:       string;
+    isByWeight:    boolean;
+    unitPrice:     string;
+    purchasePrice: string;
+    stockQuantity: string;
+}
--- src/routes/Inventario/Inventario.tsx
+++ src/routes/Inventario/Inventario.tsx
@@ -12,6 +12,7 @@ import axios from "axios";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Label } from "@radix-ui/react-label";
 import { Switch } from "@/components/ui/switch";
+import type { AddProductInterface } from "./Interfaces";
 
 
 export default function Inventario() {
@@ -28,6 +29,14 @@ export default function Inventario() {
     const [searchValue, setSearchValue] = useState<string>('');
     const refButtonSearch = useRef<HTMLButtonElement>(null);
     const refAddProduct = useRef<HTMLButtonElement>(null);
+    const [addProduct, setAddProduct] = useState<AddProductInterface>({
+        name: '',
+        skuCode: '',
+        isByWeight: false,
+        unitPrice: '',
+        purchasePrice: '',
+        stockQuantity: ''
+    });
 
     useEffect(() => {
         refetch(OFFSET_PRODUCTS);
@@ -142,6 +151,10 @@ export default function Inventario() {
                 });
             }
     }
+
+    function handleAddProduct(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+    }
     
 
     return(
@@ -166,46 +179,81 @@ export default function Inventario() {
                     <DialogContent>
                         <DialogHeader>
                         <DialogTitle>Agregar Producto</DialogTitle>
-                        <DialogDescription>
-                            <form 
-                                className="flex flex-col space-y-2">
-                                <Label>
-                                    Nombre del Producto:
-                                </Label>
-                                <Input/>
-                                <Label>
-                                    Código de Barras:
-                                </Label>
-                                <Input/>
-                                <div className="flex items-center space-x-2 mt-2 mb-2">
-                                    <Switch/>
-                                    <Label>Producto vendido por peso</Label>
-                                </div>
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div className="flex flex-col space-y-2">
-                                        <Label>
-                                            Precio por Unidad:
-                                        </Label>
-                                        <Input type="number" step="0.01" min={0}/>
-                                    </div>
-                                    <div className="flex flex-col space-y-2">
-                                        <Label>
-                                            Precio por Kilo:
-                                        </Label>
-                                        <Input type="number" step="0.01" min={0}/>
-                                    </div>
-                                </div>
-                                <Label>
-                                    Stock Inicial (unidades):
-                                </Label>
-                                <Input type="number" step="1" min={0}/>
-                                <Button 
-                                    className="bg-blue-600 hover:bg-blue-800
-                                    hover:cursor-pointer dark:text-white">
-                                    Guardar
-                                </Button>
+                        {/* El form se dejó afuera del description porque este genera una etiqueta <P></P> 
+                        y no se puede anidar un form dentro de un P */}
+                            <form onSubmit={handleAddProduct}>
+                                <DialogDescription className="flex flex-col space-y-2">
+                                    <Label htmlFor="name">
+                                        Nombre del Producto:
+                                    </Label>
+                                    <Input
+                                        id="name"
+                                        onChange={(e) => setAddProduct({...addProduct, name: e.target.value})}
+                                        value={addProduct.name}
+                                        minLength={3}
+                                        type="text"/>
+                                    <Label htmlFor="sku-code">
+                                        Código de Barras:
+                                    </Label>
+                                    <Input
+                                        id="sku-code"
+                                        onChange={(e) => setAddProduct({...addProduct, skuCode: e.target.value}) }
+                                        value={addProduct.skuCode}
+                                        minLength={3}
+                                        type="text"/>
+                                    <span className="flex items-center space-x-2 mt-2 mb-2">
+                                        <Switch 
+                                            checked={addProduct.isByWeight}
+                                            onCheckedChange={(checked) => setAddProduct({...addProduct, isByWeight: checked})}/>
+                                        <Label>Producto vendido por peso</Label>
+                                    </span>
+                                    <span className="grid grid-cols-2 gap-4">
+                                        <span className="flex flex-col space-y-2">
+                                            <Label htmlFor="unit-price">
+                                                Precio de venta:
+                                            </Label>
+                                            <Input 
+                                                className="text-right"
+                                                id="unit-price"
+                                                onChange={(e) => setAddProduct({...addProduct, unitPrice: e.target.value})}
+                                                value={addProduct.unitPrice}
+                                                type="number" 
+                                                step="0.01" 
+                                                min={0}/>
+                                        </span>
+                                        <span className="flex flex-col space-y-2">
+                                            <Label htmlFor="purchase-price">
+                                                Precio de compra:
+                                            </Label>
+                                            <Input 
+                                                className="text-right"
+                                                id="purchase-price"
+                                                onChange={(e) => setAddProduct({...addProduct, purchasePrice: e.target.value})}
+                                                value={addProduct.purchasePrice}
+                                                type="number" 
+                                                step="0.01" 
+                                                min={0}/>
+                                        </span>
+                                    </span>
+                                    <Label htmlFor="stock-quantity">
+                                        Stock Inicial (unidades):
+                                    </Label>
+                                    <Input 
+                                        className="text-right"
+                                        id="stock-quantity"
+                                        value={addProduct.stockQuantity}
+                                        onChange={(e) => setAddProduct({...addProduct, stockQuantity: e.target.value})}
+                                        type="number" 
+                                        step="1"
+                                        min={0}/>
+                                    <Button 
+                                        type="submit"
+                                        className="bg-blue-600 hover:bg-blue-800
+                                        hover:cursor-pointer dark:text-white">
+                                        Guardar
+                                    </Button>
+                                </DialogDescription>
                             </form>
-                        </DialogDescription>
                         </DialogHeader>
                     </DialogContent>
                 </Dialog>
